34361a371affafa8f3ef3736ff2af9792c67f7a3
Vitaliy <kvu15vit@rambler.ru>
2018-03-12 13:54:37 +0200

    ready

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// 2 ===============
+
+var firstName = "Alex";
+var lastName = "Alex";
+
+alert(lastName);
+
+// 3 ===============
+
+var fname = "Diana";
+var lname = " Kravchenko";
+var age = "20";
+var obrazovanie = " Высшее";
+var hobbie = " Чтение, Танцы, Боевые искусства";
+x = fname + lname;
+y = age + obrazovanie + hobbie;
+alert(x);
+alert(y);
+
+// 4 ===============
+
+var a = 1, b = 1, c, d;
+c = ++x; alert (c); // NaN
+d = b++; alert (d); // 1
+
+с = (2+ ++a); alert (c); // NaN
+d = (2+ b++); alert (d); // 4
+
+alert (a); // 2
+alert (b); // 3
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,14 @@
+
+var one = prompt('Введите 1-е число:' );
+var two = prompt('Введите 2-е число:' );
+
+var x = +one + +two;
+
+alert("Результат: " +x);
+
+
+
+
+
+
+
